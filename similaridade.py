c296fe30204b3d2c76275405e38ef00071da5e72
icrsb <icrsbarbosa@gmail.com>
2022-02-07 00:31:59 -0300

    feature: Update in QR function

--- similaridade.py
+++ similaridade.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import decomposicao as decomp
+import regressao as regre
 
 def Householder (M):
   A = M.copy()
@@ -40,7 +41,7 @@ def contructHouseholder(M, j):
 
 def QR (M, P, eps):
   A = M.copy()
-  qtdColumns = np.shape(A)
+  [qtdLines, qtdColumns] = np.shape(A)
 
   [Q, R] = decomp.decomposicaoQR(A)
   A = R @ Q
@@ -58,43 +59,50 @@ def QR (M, P, eps):
   
   diagonalMatrix = True
 
-  for i in range(qtdColumns - 1):
-    if (abs(A[i+1, i]) > eps):
-      diagonalMatrix = False
-      break
-
+  for i in range(qtdLines):
+    for j in range(qtdColumns):
+      if (abs(M[i, j] - M[j, i]) > eps):
+        print(M[i, j] - M[j, i])
+        diagonalMatrix = False
+        break
+  
   # Se a matriz é diagonal, os autovalores são os valores da diagonal
   # e os autovetores são as matrizes Qs acumuladas
   if (diagonalMatrix):
     return [[A[i, i] for i in range(qtdColumns)], P]
   else:
-    eigenValuesMatrix = []
-    eigenVectorsMatrix = np.zeros(qtdColumns)
+    eigenValues = []
+    eigenVectorsMatrix = np.zeros((qtdLines, qtdColumns))
 
     id = 0
-    while (id < qtdColumns - 1):
-      if (abs(A[id+1, id]) > eps):
-        [[a, b], [c, d]] = A[id:id+1, id:id+1]
+    while (id < qtdLines):
+      if ((id != qtdLines - 1) and (abs(A[id+1, id]) > eps)):
+        [[a, b], [c, d]] = A[id:id+2, id:id+2]
         roots = np.roots([1, -(a + d), (a * d) - (b * c)])
-        eigenValuesMatrix.append(roots[0])
-        eigenValuesMatrix.append(roots[1])
+        eigenValues.append(roots[0])
+        eigenValues.append(roots[1])
         id += 1
       else:
-        eigenValuesMatrix.append(A[id, id])
+        eigenValues.append(A[id, id])
       
       id += 1
     
     for i in range(qtdColumns):
-      autoMatrix = A - (eigenValuesMatrix[i, i] * np.eye(qtdColumns))
+      autoMatrix = A - (eigenValues[i] * np.eye(qtdColumns))
+      eigenVectorsMatrix[:, i] = regre.minQuadrado(autoMatrix, np.zeros((qtdLines, 1))).transpose()
     
-    return [eigenValuesMatrix, eigenVectorsMatrix]
+    return [eigenValues, eigenVectorsMatrix]
 
 def main ():
-  C = np.array([[2.0, 2.0, 3.0],[2.0, 3.0, 4.0],[3.0, 4.0, 6.0]])
+  C = np.array([[2.0, 2.0, 3.0],[2.0, 5.0, 4.0],[3.0, 4.0, 6.0]])
   
-  [A, H] = Householder(C)
+  #[A, H] = Householder(C)
+  [eigenValues, eigenVectors] = QR(C, np.eye(3), 0.000001)
 
-  print("Matriz:\n{}\n\nHouseholder\n{}".format(A, H))
+  #for i in range(3):
+  #  eigenVectors[:, i] = eigenVectors[:, i] / eigenVectors[2, i]
+  #print("Matriz:\n{}\n\nHouseholder\n{}".format(A, H))
+  print("Autovalores:\n{}\n\nAutovetores:\n{}".format(eigenValues, eigenVectors))
 
 if __name__ == "__main__":
   main()
